5b9f4ada71ab57532a694496a2022d2ededcc328
Zade Alfalah <69093528+zadealfalah@users.noreply.github.com>
2023-08-28 14:08:48 -0500

    start tf, schema additions

---
+++ schema.sql
@@ -0,0 +1,33 @@
+-- http://howto.philippkeller.com/2005/04/24/Tags-Database-schemas/
+-- Above to justify creation of 3 tables below just to contain tags
+-- Note that the 'Toxi' solution for tags (3 tables) is used by wordpress
+-- above also gives examples of how to query it
+
+-- https://stackoverflow.com/questions/10506181/tagging-system-toxi-solution-questions
+-- above to see some examples of how to add to such a db
+
+USE -- whatever db I end up making on AWS with terraform
+
+CREATE TABLE IF NOT EXISTS jobs (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR (150) NOT NULL,
+    company VARCHAR(150) NOT NULL,
+    location VARCHAR(150),
+    salary INT,
+    PRIMARY KEY (id)
+)
+
+
+CREATE TABLE IF NOT EXISTS tags (
+    tag_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (tag_id)
+)
+
+CREATE TABLE IF NOT EXISTS tagmap (
+    id INT NOT NULL AUTO_INCREMENT,
+    job_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    FOREIGN KEY(job_id) REFERENCES jobs.id ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES tags.tag_id ON DELETE CASCADE
+)
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+    description = "Access Key to AWS Console"
+}
+
+variable "secret_key" {
+    description = "Secret Key to AWS Console"
+}
+
+variable "region" {
+    description = "AWS Region"
+}
