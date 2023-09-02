bdbfc8b6fd7b8c79e023cabf1bef198239a0791d
zadealfalah <zadealfalah@gmail.com>
2023-09-02 15:33:42 -0500

    update jobsearchs

--- jobSearch.py
+++ jobSearch.py
@@ -1,50 +1,105 @@
-import asyncio
-import re
-import json
-from urllib.parse import urlencode
-# from arsenic import get_session
-# from arsenic.browsers import Firefox
-# from arsenic.services import Geckodriver
-from pyppeteer import launch
-
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import time
+import json
+import re
+from bs4 import BeautifulSoup as bs
+from urllib.parse import urlencode
 
 
-def parse_search_page(html: str):
-    data = re.findall(r'window.mosaic.providerData\["mosaic-provider-jobcards"\]=(\{.+?\});', html)
-    data = json.loads(data[0])
-    return {
-        "results": data["metaData"]["mosaicProviderJobCardsModel"]["results"],
-        "meta": data["metaData"]["mosaicProviderJobCardsModel"]["tierSummaries"],
-    }
+# def main():
+#     options = webdriver.FirefoxOptions()
+#     driver = webdriver.Firefox(options=options)
+
     
-async def scrape_search(client, query:str, location:str, max_results:int=50):
+    
+    
+#     jobs = scrape_jobs()
+
+
+
+def scrape_jobs(query:str, location:str):
+    options = webdriver.FirefoxOptions()
+    driver = webdriver.Firefox(options=options)
+    
     def make_page_url(offset):
-        params = {"q":query, "l":location, "filter":0, "start":offset}
-        return "https://www.indeed.com/jobs?" + urlencode(params) + ".html"
-    print(f"scraping first page of search: {query=}, {location=}")
-    print(f"URL: {make_page_url(0)}")
-    first_response_page = await client.goto(make_page_url(0))
-    # time.sleep(10)
-    first_page_data = parse_search_page(first_response_page.content())
-
-    results = first_page_data["results"]
-    total_results = sum(category["jobCount"] for category in first_page_data["meta"])
-    # there's a page limit on indeed.com of 1000 results per search
-    if total_results > max_results:
-        total_results = max_results
-    print(f"scraping remaining {total_results - 10 / 10} pages")
-    other_pages = [make_page_url(offset) for offset in range(10, total_results + 10, 10)]
-    for response in await asyncio.gather(*[client.get(url=url) for url in other_pages]):
-        results.extend(parse_search_page(response.text))
-    return results
-
-async def main():
-    browser = await launch()
-    page = await browser.newPage()
+        params = {"q": query, "l": location, "start":offset}
+        return "https://www.indeed.com/jobs?" + urlencode(params)
+    print(f"Scraping first page of search: {query=}, {location=}")
     
-    async with page as client:
-        search_data = await scrape_search(client, query='python', location='remote')
-        print(json.dumps(search_data, indent=2))
+    print(f"First URL: {make_page_url(0)}")
+    driver.get(make_page_url(0))
+    soup = bs(driver.page_source)
+    
+    results = soup.find(id='jobsearch-Main')
+    job_elements = results.find_all("div", class_="slider_item")
+    d = {}
+    for job_element in job_elements[:1]:
+        link_element_id = job_element.find_all("a")[0]["id"]
+
+        d[link_element_id] = {}
+        d[link_element_id]["title"] = job_element.find("h2", class_="jobTitle").text.strip()
+        d[link_element_id]['company'] = job_element.find("span", class_="companyName").text.strip()
+        d[link_element_id]['posted'] = job_element.find("span", class_="date").next_element.next_element.next_element
+        try:
+            d[link_element_id]['location'] = job_element.find("div", class_="companyLocation").text.strip()
+        except AttributeError: 
+            d[link_element_id]['location'] = "Not Listed"
+        try:
+            d[link_element_id]['salary'] = job_element.find("div", class_="metadata").text.strip()
+        except AttributeError:
+            d[link_element_id]['salary'] = "Not Listed"
+        # date_element = job_element.find("div", class_="visually-hidden")  # not useful as-is.  figure out if this is possible to find at all
+
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, f'//*[@id="{link_element_id}"]'))).click()
+
+
+        time.sleep(5)
         
-asyncio.run(main())
+        temp_results = soup.find("div", id='jobDescriptionText')
+        print(temp_results)
+        print(d[link_element_id]["title"], d[link_element_id]["company"])
+        # print(d[link_element_id]['title'], d[link_element_id]['company'])
+        d[link_element_id]['desc'] = temp_results.text.strip()
+    
+    return d
+
+scrape_jobs(query='python', location='remote')
+
+# options = webdriver.FirefoxOptions()
+# driver = webdriver.Firefox(options=options)
+# driver.get("https://www.indeed.com/jobs?q=data+science&l=remote")
+
+# soup = bs(driver.page_source)
+
+# results = soup.find(id="jobsearch-Main")
+# job_elements = results.find_all("div", class_="slider_item")
+
+# basePath = "https://www.indeed.com"
+# d = {}
+# # Currently using link_element_id as unique ID.  I am fairly sure it's unique - at least it has been from what I've seen so far.
+# for job_element in job_elements[:1]:
+#     link_element_id = job_element.find_all("a")[0]["id"]
+
+#     d[link_element_id] = {}
+#     d[link_element_id]["title"] = job_element.find("h2", class_="jobTitle").text.strip()
+#     d[link_element_id]['company'] = job_element.find("span", class_="companyName").text.strip()
+#     d[link_element_id]['posted'] = job_element.find("span", class_="date").next_element.next_element.next_element
+#     try:
+#         d[link_element_id]['location'] = job_element.find("div", class_="companyLocation").text.strip()
+#     except AttributeError: 
+#         d[link_element_id]['location'] = "Not Listed"
+#     try:
+#         d[link_element_id]['salary'] = job_element.find("div", class_="metadata").text.strip()
+#     except AttributeError:
+#         d[link_element_id]['salary'] = "Not Listed"
+#     # date_element = job_element.find("div", class_="visually-hidden")  # not useful as-is.  figure out if this is possible to find at all
+
+#     WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, f'//*[@id="{link_element_id}"]'))).click()
+
+#     time.sleep(2)
+#     temp_results = results.find("div", id='jobDescriptionText')
+#     # print(d[link_element_id]['title'], d[link_element_id]['company'])
+#     d[link_element_id]['desc'] = temp_results.text.strip()
