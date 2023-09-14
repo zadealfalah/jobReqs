0ccd3597f44d0aa51d7912cf72fb270572499b0f
zadealfalah <zadealfalah@gmail.com>
2023-09-14 11:52:38 -0500

    add 14-09 data, update dag

--- job_dag.py
+++ job_dag.py
@@ -20,16 +20,29 @@ default_args = {
 
 dag = DAG(
     'test',
-    default_args=default_args,x
+    default_args=default_args,
     description='Test DAG',
     schedule_interval=timedelta(days=1),
     start_date=days_ago(1), #set this to start 12:01 on whatever day when I actually launch it
     tags=['testing']
 )
 
+get_jobs_script = fr"jobSearch.py"
+add_techs_script = fr"add_techs.py"
+
+
+
 #Used to get the raw_data-{date}.json files
 t1 = BashOperator(
-    task_id="get_raw_data",
-    bash_command=f"python {os.getenv('path_to_jobsearch')}",
+    task_id="get_jobs",
+    bash_command=f"python {get_jobs_script}",
     dag=dag
 )
+
+t2 = BashOperator(
+    task_id="add_techs",
+    bash_command=f"python {add_techs_script}",
+    dag=dag
+)
+
+t1 >> t2
