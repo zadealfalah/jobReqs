2e3af24147450a8205b1771b5e81f8547e0fd295
zadealfalah <zadealfalah@gmail.com>
2023-10-31 13:52:08 -0500

    update hf, check results, job search

--- job_search.py
+++ job_search.py
@@ -15,6 +15,7 @@ import numpy as np
 from utils import get_url, dict_to_json, get_job_data
 from globals import k
         
+from collections import deque, defaultdict
 
 load_dotenv()        
 
@@ -34,6 +35,7 @@ max_threads = int(os.getenv("max_threads"))
 num_pages = int(os.getenv("num_pages"))
 num_iters = num_pages // max_threads
 keyword_list = json.loads(os.getenv("keyword_list"))
+# keyword_list = ['mlops'] # for testing
 location_list = json.loads(os.getenv("location_list"))
 days_ago = os.getenv("days_ago")
 
@@ -68,69 +70,80 @@ threads, driver_list = create_threaded_drivers()
 end_create_drivers = time.time()
 
 
-def get_job_ids(driver, keyword, location, offset, days_ago, current_iter_job_ids):
-    job_id_list=k['job_id_list']
+# Create an event object to signal the threads to stop
+stop_event = threading.Event()
+
+# Create a dictionary to track job keys for each keyword and location
+job_keys_dict = defaultdict(set)
+
+# Create a deque to keep track of the last 10 job keys
+last_10_job_keys = deque(maxlen=10)
+
+def get_job_ids(driver, keyword, location, offset, days_ago):
+    job_id_list = k['job_id_list']
     indeed_jobs_url = get_url(keyword, location, offset, days_ago)
     try:
         driver.get(indeed_jobs_url)
-        # time.sleep(np.random.uniform(1, 2))
-        response = driver.page_source  # get the html of the page
+        response = driver.page_source
         script_tag = re.findall(r'window.mosaic.providerData\["mosaic-provider-jobcards"\]=(\{.+?\});', response)
         if script_tag is not None:
             json_blob = json.loads(script_tag[0])
             jobs_list = json_blob['metaData']['mosaicProviderJobCardsModel']['results']
+
             for i, job in enumerate(jobs_list):
-                if (job.get('jobkey') is not None) & (job.get('jobkey') not in job_id_list):
-                    job_id_list.append((job.get('jobkey'), keyword))
-                    current_iter_job_ids.append(job.get('jobkey'))
+                if stop_event.is_set():
+                    return  # Exit the function if the event is set
+
+                if job.get('jobkey') is not None:
+                    job_key = job.get('jobkey')
+                    print(job_key)
+                    if job_key in last_10_job_keys:
+                        # Detected repeating job keys in the last 10 jobs, set the stop event
+                        stop_event.set()
+                        return  # Exit the function
+
+                    last_10_job_keys.append(job_key)
+                    job_keys_dict[(keyword, location)].add(job_key)
+                    job_id_list.append((job_key, keyword))
+
     except Exception as e:
         print("Error", e)
-        
-        
 
-        
+# Iterate through your loops
 for keyword in keyword_list:
     for location in location_list:
-        stops = False
-        # print(f"Searching for {keyword} in {location}")
+        stop_event.clear()  # Clear the stop event for each new location
+        job_keys_dict[(keyword, location)] = set()  # Clear the job keys set for each new location
+        last_10_job_keys.clear()  # Clear the last 10 job keys for each new location
+        
+        found_repeating_job_keys = False  # Flag to track if repeating job keys were found
+        
         for i in range(0, num_iters):
-            if stops == True:
-                break
-            else:
-                prev_iter_job_ids = []
-                for j in range(0, max_threads):
-                    current_iter_job_ids = []
-                    offset = i*10*max_threads + j*10
-                    print(f"Searching for {keyword} in {location} on page {int((offset/10)+1)}")
-                    t = threading.Thread(args=(driver_list[j], keyword, location, offset, days_ago, current_iter_job_ids), target=get_job_ids) 
-                    t.start()
-                    threads.append(t)
+            threads = []
 
-                    ## If set here both lists are always empty, instantly finishes.
-                    # diffs = np.setdiff1d(current_iter_job_ids, prev_iter_job_ids)
-                    # if len(diffs) == 0:
-                    #     print(current_iter_job_ids, prev_iter_job_ids)
-                    #     print(f"No diff between last and current job IDs on page {int((offset/10)+1)}.")
-                    #     stops = True
-                    #     break 
-                    # else:
-                    #     prev_iter_job_ids = current_iter_job_ids
-                        
-                for t in threads:
-                    t.join()
-                ## If set here, will only compare page 10 to 11, 20 to 21, etc.
-                # diffs = np.setdiff1d(current_iter_job_ids, prev_iter_job_ids)
-                # if len(diffs) == 0:
-                #     print(current_iter_job_ids, prev_iter_job_ids)
-                #     print(f"No diff between last and current job IDs on page {int((offset/10)+1)}.")
-                #     stops = True
-                #     break 
-                # else:
-                #     prev_iter_job_ids = current_iter_job_ids
+            for j in range(0, max_threads):
+                offset = i * 10 * max_threads + j * 10
+                print(f"Searching for {keyword} in {location} on page {int((offset/10) + 1)}")
+                t = threading.Thread(args=(driver_list[j], keyword, location, offset, days_ago), target=get_job_ids)
+                t.start()
+                threads.append(t)
 
-                    
-# print(f"Threads results:")
-# print([thread.result() for thread in threads]) #doesn't work, how to set the old_jobs_flag with a thread??
+            # Join the threads to wait for them to finish
+            for t in threads:
+                t.join()
+
+            if stop_event.is_set():
+                found_repeating_job_keys = True  # Set the flag if repeating job keys were found
+                break  # Exit the inner loop
+
+        if found_repeating_job_keys:
+            print(f"Found repeating job keys.")
+            # Exit the outer loop to move to the next location
+            break
+
+
+
+
 end_find_jobs = time.time()
 
 print(f"Found {len(job_id_list)} job combos.")
