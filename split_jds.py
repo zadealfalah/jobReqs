c08c79b387bc1818aba96975e6871fa5de1da906
zadealfalah <zadealfalah@gmail.com>
2024-02-06 13:19:34 -0600

    Update tech id pipeline

--- split_jds.py
+++ split_jds.py
@@ -3,13 +3,26 @@ import json
 import spacy
 import pickle
 import os
+import logging
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class TechIdentificationPipeline:
     def __init__(self, filename):
         self.filename = filename
         self.data = []
         self.nlp = spacy.load("en_core_web_sm")
-        self.clf = os.
+        self.clf = None
+        self.tfidf = None
+        
+        self.logger = logging.getLogger("TechIdentificationPipeline")
+        self.logger.setLevel(logging.INFO)
+        file_handler = logging.FileHandler("TechIdPipeline.log")
+        file_handler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
         
     def read_data_lines(self):
         """Stores each line (job) into self.data
@@ -19,19 +32,35 @@ class TechIdentificationPipeline:
             data (_type_): _description_
         """
         if not self.filename:
-            print(f"Error, no filename detected!")
+            self.logger.info(f"Error, no filename detected!")
         else:
             try:
-                print(f"Parsing {self.filename} to self.data")
+                self.logger.info(f"Parsing {self.filename} to self.data")
                 with open(rf"{self.filename}") as f:
                     for line in f:
                         self.data += [json.loads(line)]
             except Exception as e:
-                print(f"Error parsing {self.filename}: {e}")
+                self.logger.info(f"Error parsing {self.filename}: {e}")
             
+    
+    def read_in_clf(self):
+        try:
+            with open(fr"{os.getenv('SAVED_CLF')}", "rb") as model_file:
+                self.clf = pickle.load(model_file)
+            self.logger.info(f"Clf read in")
+        except Exception as e:
+            self.logger.info(f"Error reading in clf: {e}")
+    
+    def read_in_tfidf(self):
+        try: 
+            with open(fr"{os.getenv('SAVED_TFIDF')}", "rb") as vect_file:
+                self.tfidf = pickle.load(vect_file)
+            self.logger.info(f"Tfidf read in")
+        except Exception as e:
+            self.logger.info(f"Error reading in tfidf: {e}")
+    
         
-        
-    def select_relevant_text(self, vectorizer, clf, nlp, n=5):
+    def select_relevant_text(self, n=5):
         """Uses pretrained binary classifier to decide if a given section of jd is relevant to techs.  
         Used to shorted number of tokens for GPT.  Adds split jds directly to each job in self.data
 
@@ -46,21 +75,29 @@ class TechIdentificationPipeline:
             _type_: _description_
         """
         if self.data == []:
-            print(f"Data field is empty! Reading in data from {self.filename}")
+            self.logger.info(f"Data field is empty! Reading in data from {self.filename}")
             self.read_data_lines()
         
+        if self.clf == None:
+            self.logger.info(f"Clf not yet read in.  Reading in now.")
+            self.read_in_clf()
+            
+        if self.tfidf == None:
+            self.logger.info(f"Tfidf not yet read in.  Reading in now.")
+            self.read_in_tfidf()
         
-        print(f"Data sucessfully read in, updating jds")
+        
+        self.logger.info(f"Data, clf, tfidf all successfuly loaded. Updating jds")
         modified_data = []
         for job in self.data:
             original_jd = job['job_description']
             # Don't use split function as we don't want the output I use to label them manually.
             splits = original_jd.count("\n")//n
             split_text = re.findall("\n".join(["[^\n]+"]*splits), original_jd)
-            processed = [" ".join([token.lemma_ for token in nlp(para)]) for para in split_text]
+            processed = [" ".join([token.lemma_ for token in self.nlp(para)]) for para in split_text]
             
-            transformed = vectorizer.transform(processed)
-            pred_vals = clf.predict(transformed)
+            transformed = self.tfidf.transform(processed)
+            pred_vals = self.clf.predict(transformed)
             
             zipped_paras = list(zip(split_text, pred_vals))
             # return(zipped_paras)
@@ -68,7 +105,7 @@ class TechIdentificationPipeline:
             job['split_jd'] = split_jd.strip("\n")  # add on stripping the newlines from the cleaned descs to lower # tokens
             modified_data.append(job) # Store the whole job in the modified data list
         
-        print(f"Rewriting {self.filename} with updated jobs")
+        self.logger.info(f"Rewriting {self.filename} with updated jobs")
         with open(self.filename, 'w') as f:
             for job in modified_data:
                 json.dump(job, f)
