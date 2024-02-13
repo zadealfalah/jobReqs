b82fd50092df831cc5ae7fcb54f8a4b5a770ad57
zadealfalah <zadealfalah@gmail.com>
2024-02-12 18:25:59 -0600

    moved models, updated tech pipeline

--- tech_pipeline.py
+++ tech_pipeline.py
@@ -20,9 +20,10 @@ class TechIdentificationPipeline:
         self.filename = filename
         self.data = []
         self.nlp = spacy.load("en_core_web_sm")
-        self.clf = None
-        self.tfidf = None
+        self.clf = None # Don't read in automatically
+        self.tfidf = None # Don't read in automatically
         
+        ## Logging
         self.logger = logging.getLogger("TechIdentificationPipeline")
         self.logger.setLevel(logging.INFO)
         file_handler = logging.FileHandler("TechIdPipeline.log")
@@ -31,7 +32,7 @@ class TechIdentificationPipeline:
         file_handler.setFormatter(formatter)
         self.logger.addHandler(file_handler)
         
-        
+        ## Gpt
         self.OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
         self.GPT_MODEL=os.getenv("GPT_MODEL")
         self.GPT_PROMPT=os.getenv("GPT_PROMPT")
@@ -40,8 +41,11 @@ class TechIdentificationPipeline:
         self.EXAMPLE_TEXT_2=os.getenv("EXAMPLE_TEXT_2")
         self.EXAMPLE_RESPONSE_2=os.getenv("EXAMPLE_RESPONSE_2")
         self.EXAMPLE_PROMPT=os.getenv("EXAMPLE_PROMPT")
-        
         self.system_prompt = f"{self.EXAMPLE_TEXT_1}\n{self.EXAMPLE_RESPONSE_1}\n{self.EXAMPLE_TEXT_2}\n{self.EXAMPLE_RESPONSE_2}\n"
+        
+        ## Cleaning
+        self.term_mapping = None # Don't read in automatically
+        self.terms_to_remove = None # Don't read in automatically
         
     def read_data_lines(self):
         """Stores each line (job) into self.data
@@ -61,22 +65,49 @@ class TechIdentificationPipeline:
             except Exception as e:
                 self.logger.info(f"Error parsing {self.filename}: {e}")
             
-    
+    ###### Should add something to allow choices between various saved clf/tfidfs by version number.  Not needed for now
     def read_in_clf(self):
         try:
-            with open(fr"{os.getenv('SAVED_CLF')}", "rb") as model_file:
-                self.clf = pickle.load(model_file)
-            self.logger.info(f"Clf read in")
+            for root, dirs, files in os.walk('.'):
+                if 'classifier_models' in dirs:
+                    c_models_folder = os.path.join(root, 'classifier_models')
+                    self.logger.info("classifier_models folder found")
+                    for filename in os.listdir(c_models_folder):
+                        if filename.startswith("job_desc_classifier_"):
+                            filepath = os.path.join(c_models_folder, filename)
+                            with open(filepath, "rb") as model_file:
+                                self.clf = pickle.load(model_file)
+                                self.logger.info("Classifier loaded")
+                            return  # Exit the function after loading the classifier
+                    else:
+                        self.logger.error("No classifier file found in classifier_models folder")
+                        return
+            else:
+                self.logger.error("classifier_models folder not found")
         except Exception as e:
-            self.logger.info(f"Error reading in clf: {e}")
+            self.logger.error(f"Error reading in clf: {e}")
     
     def read_in_tfidf(self):
-        try: 
-            with open(fr"{os.getenv('SAVED_TFIDF')}", "rb") as vect_file:
-                self.tfidf = pickle.load(vect_file)
-            self.logger.info(f"Tfidf read in")
+        try:
+            for root, dirs, files in os.walk('.'):
+                if 'classifier_models' in dirs:
+                    c_models_folder = os.path.join(root, 'classifier_models')
+                    self.logger.info("classifier_models folder found")
+                    for filename in os.listdir(c_models_folder):
+                        if filename.startswith("job_desc_tfidf_vectorizer_"):
+                            filepath = os.path.join(c_models_folder, filename)
+                            with open(filepath, "rb") as model_file:
+                                self.tfidf = pickle.load(model_file)
+                                self.logger.info("Tfidf loaded")
+                            return  # Exit the function after loading the classifier
+                    else:
+                        self.logger.error("No tfidf file found in classifier_models folder")
+                        return
+            else:
+                self.logger.error("classifier_models folder not found")
         except Exception as e:
-            self.logger.info(f"Error reading in tfidf: {e}")
+            self.logger.error(f"Error reading in tfidf: {e}")
+    
     
         
     def select_relevant_text(self, n=5):
@@ -132,11 +163,12 @@ class TechIdentificationPipeline:
             for job in self.data:
                 json.dump(job, f)
                 f.write('\n')
-
-
-
     
-    # @backoff.on_exception(backoff.expo, openai.RateLimitError, max_tries=6)
+    # class RateLimitError(Exception):
+    #     pass
+    
+    # def log_attempt_number(self, retry_state):
+    #     self.logger.warning(f"Retrying GPT: {retry_state.attempt_number}...")
     @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
     async def ask_gpt(self, split_jd):
         headers = {
@@ -152,16 +184,20 @@ class TechIdentificationPipeline:
                         {"role": "user", "content": f"{self.EXAMPLE_PROMPT} {split_jd}"}
                     ]
                 }) as response:
-                    return await response.json()
+                    response_json = await response.json()
+                    if response_json['error']:
+                        self.logger.warning(f"Rate limit exceeded in ask_gpt method")
+                        raise Exception
+                    return response_json
             except ClientResponseError as e:
                 if e.status == 429:  # Rate limit exceeded error
-                    logging.warning(f"Rate limit exceeded in ask_gpt method: {e}")
+                    self.logger.warning(f"Rate limit exceeded in ask_gpt method: {e}")
                     raise e
                 else:
-                    logging.warning(f"Unexpected error occurred in ask_gpt method: {e}")
+                    self.logger.warning(f"Unexpected error occurred in ask_gpt method: {e}")
                     raise e
             except Exception as e:
-                logging.warning(f"Unexpected error occurred in ask_gpt method: {e}")
+                self.logger.warning(f"Unexpected error occurred in ask_gpt method: {e}")
                 raise e
 
     async def fetch_gpt_techs(self):
@@ -190,16 +226,17 @@ class TechIdentificationPipeline:
             common_response = "Return specific tools and technologies from the following text: "
             if gpt_message.startswith("["): # Starts with a list
                 job['gpt_techs'] = gpt_message.lower()
-                self.logger.info(f"GPT techs {gpt_message.lower()} added to job {job['job_key']}")
+                # self.logger.info(f"GPT techs {gpt_message.lower()} added to job {job['job_key']}")
             elif gpt_message.startswith(common_response): # Starts with command given
                 self.logger.info(f"GPT Response includes command, cleaning")
                 gpt_techs = gpt_message[len(common_response):].lower().split(", ")
                 job['gpt_techs'] = gpt_techs
-                self.logger.info(f"GPT techs {gpt_techs} added to job {job['job_key']}")
+                # self.logger.info(f"GPT techs {gpt_techs} added to job {job['job_key']}")
             else: # Some other kind of incorrect response
                 self.logger.info(f"GPT Response is non-standard")
                 job['gpt_techs'] = None
                 self.logger.warning(f"GPT techs for {job['job_key']} non-standard, added None to gpt_techs")
+                self.logger.warning(f"GPT response was: {gpt_message}")
                 
         self.logger.info(f"Overwriting {self.filename} with updated data")
         with open(self.filename, 'w') as f:
@@ -207,3 +244,87 @@ class TechIdentificationPipeline:
                 json.dump(job, f)
                 f.write('\n')
         self.logger.info(f"Data overwritten")
+        
+        
+    ## Cleaning tech list section
+    def read_in_cleaning(self):
+        try:
+            for root, dirs, files in os.walk('.'):
+                if 'cleaning_resources' in dirs:
+                    clean_folder = os.path.join(root, 'cleaning_resources')
+                    self.logger.info("cleaning_resources folder found")
+                    term_mapping_found = False
+                    terms_to_remove_found = False
+                    for filename in os.listdir(clean_folder):
+                        if filename.startswith("term_mapping"):
+                            filepath = os.path.join(clean_folder, filename)
+                            with open(filepath, "rb") as file:
+                                self.term_mapping = json.load(file)
+                                term_mapping_found = True
+                        elif filename.startswith("terms_to_remove"):
+                            filepath = os.path.join(clean_folder, filename)
+                            with open(filepath, "rb") as file:
+                                self.terms_to_remove = json.load(file)
+                                terms_to_remove_found = True
+                                
+                    if term_mapping_found:
+                        self.logger.info("Term mapping loaded")
+                    else:
+                        self.logger.warning("Error loading term mapping")
+                    if terms_to_remove_found:
+                        self.logger.info("Terms to remove loaded")
+                    else:
+                        self.logger.warning("Error loading terms to remove")
+                    return
+                    
+            else:
+                self.logger.error("classifier_models folder not found")
+        except Exception as e:
+            self.logger.error(f"Error reading in cleaning data: {e}")          
+
+
+    def map_term(self, tech, term_mapping):
+        for key, mapped_terms in term_mapping.items():
+            for mapped_term in mapped_terms:
+                if re.match(r'\b' + re.escape(mapped_term) + r'\b', tech, re.I):
+                    return key
+        return tech
+
+    def should_remove_partial(self, tech, to_remove):
+        tech_lower = tech.lower()
+        for keyword in to_remove:
+            if re.search(r'\b' + re.escape(keyword) + r'\b', tech_lower, re.I):
+                return True
+        return False
+
+    def should_remove_exact(self, tech, to_remove):
+        tech_lower = tech.lower()
+        for keyword in to_remove:
+            if tech_lower == keyword.lower():
+                return True
+        return False
+
+
+    def clean_tech_lists(self):
+        if (self.term_mapping == None) | (self.terms_to_remove == None):
+            self.logger.info(f"No cleaning resources available, loading in now")
+            self.read_in_cleaning()
+    
+        self.logger.info(f"Cleaning gpt tech lists with cleaning resources")
+        for job in self.data:
+            gpt_tech_list = job['gpt_techs']
+            cleaned_techs = []
+            unique_techs = set() # Mapping ['aws glue', 'aws kinesis'] would give two ['aws'], so we want the uniques
+            print(f"{job['job_key']}, {gpt_tech_list}")
+            for tech in gpt_tech_list:
+                if not self.should_remove_exact(tech, self.terms_to_remove['to_remove_exact']) and not self.should_remove_partial(tech, self.terms_to_remove['to_remove_partial']):
+                    mapped_term = self.map_term(tech, self.term_mapping)
+                    if mapped_term not in unique_techs:
+                        unique_techs.add(mapped_term)
+                        cleaned_techs.append(mapped_term)
+                    else:
+                        continue
+                else:
+                    continue
+            job['cleaned_techs'] = cleaned_techs
+        self.logger.info(f"Added leaned techs to job {job['job_key']}")
